615924c66d457917520bed3cc20c629d9ee0468d
i-eva <ievaswanson@u.boisestate.edu>
2024-03-20 18:19:19 -0500

    exercise for react part 3

--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,12 +1,16 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
-         <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
-         </ol>
+         <h2>Cold Connections</h2>
+         <div>
+            <p>The characteristic most metal workers explore is plasticity.</p>
+            <p>Metal can be melted, molded, pulled, bent, curved...</p>
+            <p></p>
+            <p>...Metal can also be worked cold.</p>
+            <p>I've drawn wire, hammered rivets, woven wire, etched and cut sheet.</p>
+            <p>I studied patinas, dyes, and electrical coloration techniques.</p>
+            <p>The characteristics I explored were color and structure.</p>
+         </div>
       </div>
    );
 }
